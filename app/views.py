bfd80d934243caa6f2fa625a726cba884e131b07
Sushmadavasam <sushmada878@gmail.com>
2023-06-07 19:08:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def srujana(request):
+    return HttpResponse('<marquee><h1> TINNAVA RA ,VADILESTUNNAVA...........</h1></marquee>')
